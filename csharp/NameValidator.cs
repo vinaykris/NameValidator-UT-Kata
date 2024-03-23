5bbaa4c44626c9e846dac35b4735a90002ce469c
Vinay Krishna <vinaykris@gmail.com>
2024-03-23 17:17:12 +0530

    Add files via upload

---
+++ csharp/NameValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NameValidator_UT_Kata
+{
+    public class NameValidator
+    {
+        private const string regex = "^[a-zA-Z0-9]+$";
+
+        public bool ValidateUserName(string name)
+        {
+            if (name.Length < 6 || name.Length > 12) return false;
+            // initializes the regex pattern, gives it the username, and returns true or false if they match
+            return Regex.IsMatch(name, regex);
+        }
+    }
+}
